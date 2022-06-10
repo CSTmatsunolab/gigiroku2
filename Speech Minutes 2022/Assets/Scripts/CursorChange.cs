656d3515543439364b2d4a81e3960343f6de9839
csta22001 <103982334+csta22001@users.noreply.github.com>
2022-06-10 14:10:21 +0900

    カーソルボタンの不具合修正

--- Speech Minutes 2022/Assets/Scripts/CursorChange.cs
+++ Speech Minutes 2022/Assets/Scripts/CursorChange.cs
@@ -6,15 +6,19 @@ public class CursorChange : MonoBehaviour
 {
     public Texture2D RedCursor;
     
-    int pointerjudge=0;
+    int pointerjudge = 0;
+
         public void OnclickCursorButton()
     {
-        if(pointerjudge==0){
+        if(pointerjudge == 0)
+        {
             Cursor.SetCursor(RedCursor,new Vector2(RedCursor.width / 2,RedCursor.height / 2), CursorMode.Auto);
-            pointerjudge=1;
-        }else{
+            pointerjudge = 1;
+        }
+        else
+        {
             Cursor.SetCursor(null,Vector2.zero, CursorMode.Auto);
-            pointerjudge=0;
+            pointerjudge = 0;
         }
     }
 }
