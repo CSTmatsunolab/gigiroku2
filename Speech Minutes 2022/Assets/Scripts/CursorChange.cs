0a812bcbfe9e6e47fc3dc32fc7e005113446dea8
andoureo <65376120+andoureo@users.noreply.github.com>
2022-11-21 10:53:36 +0900

    ポインター切り替え安定

--- Speech Minutes 2022/Assets/Scripts/CursorChange.cs
+++ Speech Minutes 2022/Assets/Scripts/CursorChange.cs
@@ -5,74 +5,103 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using MonobitEngine;
 
-public class CursorChange :  MonobitEngine.MonoBehaviour
+public class CursorChange : MonobitEngine.MonoBehaviour
 {
 
-      [SerializeField]
+    [SerializeField]
     GameObject CursorImage;
 
     [SerializeField]
     GameObject canvas;
 
     public Texture2D RedCursor;
-    
+
     public int pointerjudge = 0;
 
     public GameObject Panel;
 
-public void Start() {
+    public bool Always = false;
 
-}
-        public void OnclickPointerButton_Always()
+    public bool Only = false;
+
+    public Button Alwaysbtn;
+
+    public Button Onlybtn;
+
+    GameObject prefab;
+    public void Start()
     {
-        /*
-        if(pointerjudge == 0)
-        {
-            Cursor.SetCursor(RedCursor,new Vector2(RedCursor.width / 2,RedCursor.height / 2), CursorMode.Auto);
-            pointerjudge = 1;
-        }
-        else
-        {
-            Cursor.SetCursor(null,Vector2.zero, CursorMode.Auto);
-            pointerjudge = 0;
-        }
-        */
-        if(pointerjudge == 0)
-        {
-            pointerjudge = 1;
-            Debug.Log(pointerjudge);
-            GameObject prefab = MonobitEngine.MonobitNetwork.Instantiate("CursorPointer", Vector3.zero, Quaternion.identity, 0);
-            //prefab.transform.SetParent(canvas.transform, false);
-            Debug.Log("CursorPointer複製完了");
-            //Cursor.visible = false;
-            
-            
-        }
-        else
-        {
-             //Cursor.visible = true;
-             if(pointerjudge!=2)
-            pointerjudge = 0;
-            Debug.Log(pointerjudge);
-        }
+
     }
-         public void OnclickPointerButton_Only()
+    public void Update()
     {
-        if(pointerjudge!=2){
-        pointerjudge=2;
         Debug.Log(pointerjudge);
-        }else{
-            pointerjudge = 0;
-            Debug.Log(pointerjudge);
+    }
+    public void OnclickPointerButton_Always()
+    {
+        pointerjudge = 0;
+        Always = !Always;
+        if (Always == true)
+        {
+            Only = false;
+            Onlybtn.interactable = false;
+            prefab = MonobitEngine.MonobitNetwork.Instantiate("CursorPointer", Vector3.zero, Quaternion.identity, 0);
+            //prefab.transform.SetParent(canvas.transform, false);
+            Debug.Log("CursorPointer複製");
+            //Cursor.visible = false;
+        }
+        else
+        {
+            pointerjudge = 1;
+            Onlybtn.interactable = true;
         }
     }
-    
 
-    public void OnPointerEnter(){
-Panel.SetActive(true);
+    public void OnclickPointerButton_Only()
+    {
+        pointerjudge = 0;
+        Only = !Only;
+        if (Only == true)
+        {
+            Always = false;
+            Alwaysbtn.interactable = false;
+        }
+        else
+        {
+            Alwaysbtn.interactable = true;
+        }
     }
 
-    public void OnPointerExit(){
+    public void OnPD()
+    {
+        Debug.Log("生きてますよ");
+        pointerjudge = 0;
+        if (Alwaysbtn.interactable == false)
+        {
+            prefab = MonobitEngine.MonobitNetwork.Instantiate("CursorPointer", Vector3.zero, Quaternion.identity, 0);
+            Debug.Log("CursorPointer複製");
+
+        }
+    }
+
+    public void OnPU()
+    {
+        Debug.Log("生きてますよ");
+        if (Alwaysbtn.interactable == false)
+        {
+            pointerjudge = 1;
+        }
+
+    }
+
+
+    public void OnPointerEnter()
+    {
+        Panel.SetActive(true);
+    }
+
+    public void OnPointerExit()
+    {
         Panel.SetActive(false);
     }
 }
--- Speech Minutes 2022/Assets/Scripts/CursorControll.cs
+++ Speech Minutes 2022/Assets/Scripts/CursorControll.cs
@@ -7,64 +7,54 @@ using MonobitEngine;
 
 public class CursorControll : MonobitEngine.MonoBehaviour
 {
-     GameObject Pointer;
+    GameObject Pointer;
     CursorChange CursorChange;
-    GameObject Plane;
-    CursorforOnlyDrawing CursorforOnlyDrawing;
- Vector3 mousePos ;
- public Text Text;
-        
+    Vector3 mousePos;
+    public Text Text;
 
-   void Start() {
-        if (monobitView.isMine){
-    mousePos=Input.mousePosition;
 
-     Pointer = GameObject.Find("Pointer");
-    CursorChange = Pointer.GetComponent<CursorChange>();
-
-    Plane = GameObject.Find("Plane");
-    CursorforOnlyDrawing = Plane.GetComponent<CursorforOnlyDrawing>();
-    Text.text="";
+    void Start()
+    {
+        Pointer = GameObject.Find("Pointer");
+        CursorChange = Pointer.GetComponent<CursorChange>();
+        // if (monobitView.isMine){
+        mousePos = Input.mousePosition;
+        Text.text = "";
         //自分のユーザーネーム取得ののち送信
-        Text.text=MonobitEngine.MonobitNetwork.player.name;
+        Text.text = MonobitEngine.MonobitNetwork.player.name;
         string text_ = Text.text;
         monobitView.RPC("UsernameForPointer", MonobitTargets.OthersBuffered, text_);
-
-        }
-
-        
-  }
-  
-
-  
-
-   [MunRPC]
+        //}
+    }
+    [MunRPC]
     public void UsernameForPointer(string text_)
     {
         Text.text = text_;
     }
-    
-    // Update is called once per frame
-    void Update () {
-        if (monobitView.isMine){
-        if(CursorChange.pointerjudge ==0){
-            OnDestroy();
-            Debug.Log("ポインタ削除済み");
-        }else if(CursorChange.pointerjudge ==4){
-                CursorChange.pointerjudge=2;
-                OnDestroy();
-                Debug.Log("ポインタ削除済み");
-                
-            }
-        }
-        
-         mousePos = Input.mousePosition;
-            this.transform.position=mousePos;
-             //Debug.Log(mousePos);
-        }
-           
 
-        public void OnDestroy()
+    // Update is called once per frame
+    void Update()
+    {
+        /* if(CursorChange.pointerjudge ==0){
+             OnDestroy();
+             Debug.Log("ポインタ削除済み");
+         }else if(CursorChange.pointerjudge ==4){
+                 CursorChange.pointerjudge=2;
+                 OnDestroy();
+                 Debug.Log("ポインタ削除済み");
+
+         }*/
+        if (CursorChange.pointerjudge == 1)
+        {
+            OnDestroy();
+        }
+        mousePos = Input.mousePosition;
+        this.transform.position = mousePos;
+        //Debug.Log(mousePos);
+    }
+
+
+    public void OnDestroy()
     {
         MonobitNetwork.Destroy(monobitView);
     }
