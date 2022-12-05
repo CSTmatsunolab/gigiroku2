7bd471c9ca393c557942fe34f531ed3a23ab6f2f
andoureo <65376120+andoureo@users.noreply.github.com>
2022-12-05 10:08:56 +0900

    テキストチャットテスト

---
+++ Speech Minutes 2022/Assets/Scripts/TextChat.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MonobitEngine;
+using UnityEngine.SceneManagement;
+using MonobitEngine.VoiceChat;
+using UnityEngine.UI;
+public class TextChat : MonobitEngine.MonoBehaviour
+{
+
+    public CanvasGroup textChatAlpha;
+
+    public GameObject textchatPrefab;
+
+    public RectTransform content;
+
+    public InputField inputField;
+
+
+
+    public
+    // Start is called before the first frame update
+    void Start()
+    {
+        textChatAlpha.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickSendBtn()
+    {
+        // ルームに入室している場合
+        if (MonobitNetwork.inRoom)
+        {
+            /*GameObject text = Instantiate(textchatPrefab, content);
+            text.GetComponent<Text>().text = MonobitEngine.MonobitNetwork.player.name + " " + inputField.text;
+            text.GetComponent<Text>().text.Replace(" ", "\u00A0");*/
+            monobitView.RPC("TextChatrcv", MonobitTargets.AllBuffered, inputField.text, MonobitEngine.MonobitNetwork.player.name);
+            inputField.text = "";
+        }
+        else { return; }
+    }
+    [MunRPC]
+    public void TextChatrcv(string textcontent, string name)
+    {
+        GameObject text = Instantiate(textchatPrefab, content);
+        text.GetComponent<Text>().text = name + " " + textcontent;
+        text.GetComponent<Text>().text.Replace(" ", "\u00A0");
+    }
+    public void OnClickCloseBtn()
+    {
+        textChatAlpha.alpha = 0;
+    }
+    public void OnClickTextChatBtn()
+    {
+        if (textChatAlpha.alpha == 1)
+        {
+            textChatAlpha.alpha = 0;
+        }
+        else
+        {
+            textChatAlpha.alpha = 1;
+        }
+    }
+
+}
