886df2944ad8bdc05bc3aadfe358ddb3bb45da48
andoureo <65376120+andoureo@users.noreply.github.com>
2023-02-24 08:59:28 +0900

    あ

---
+++ Speech Minutes 2022/Assets/Scripts/FusenPositionShare.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+using System.Linq;
+using UnityEngine.UI;
+public class FusenPositionShare : MonobitEngine.MonoBehaviour
+{
+
+    public GameObject fusenpanel;
+    public void OnOtherPlayerConnected(MonobitEngine.MonobitPlayer newPlayer)
+    {
+        monobitView.RPC("sharepos", MonobitEngine.MonobitTargets.Others, newPlayer.ID, fusenpanel.transform.position);
+    }
+
+    [MunRPC]
+    public void sharepos(int id, Vector3 pos)
+    {
+        if (id == MonobitEngine.MonobitNetwork.player.ID)
+        {
+            fusenpanel.transform.position = pos;
+        }
+        else { return; }
+    }
+}
